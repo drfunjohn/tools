5bbf66c1afd0269c77b31149626f5f854884874a
Evgeny Sherbakov <e.sherbakov@f5.com>
2020-11-18 18:37:28 +0200

    chore: remove no needed files

--- gitlab-runner-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-gitlab-runner stop
-sudo curl --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
-sudo chmod +x /usr/local/bin/gitlab-runner
-cd ~
-gitlab-runner start
